12ec846319e987c3843d4a600a3b36cd44ec1156
FedorProgrammer123 <fedorlohkin@gmail.com>
2025-03-10 14:45:37 +0300

    DatabaseMasterPol

---
+++ MasterPolApp/MasterPolApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterPolApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+                if (string.IsNullOrEmpty(LoginTextBox.Text))
+                {
+                    errors.AppendLine("Введите логин!");
+                }
+                if (string.IsNullOrEmpty(PasswordTextBox.Password))
+                {
+                    errors.AppendLine("Введите пароль!");
+                }
+                if(errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+                if(Data.DatabaseMasterPolEntities.GetContext().Users.Any(d => d.UserLogin == LoginTextBox.Text
+                && d.UserPassword == PasswordTextBox.Password))
+                {
+                    MessageBox.Show("Успех!", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+       
+        }
+    }
+}
